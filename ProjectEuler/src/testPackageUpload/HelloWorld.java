738af3beb2064f24fe14807daa932fc7c1156e5d
Fred Skallos <fredska@gmail.com>
2011-07-20 16:22:22 -0400

    First upload via Eclipse

---
+++ ProjectEuler/src/testPackageUpload/HelloWorld.java
@@ -0,0 +1,18 @@
+package testPackageUpload;
+
+/**
+ * Just a simple class to upload test to github for the first time
+ * using egit (Eclipse & git plugin)
+ * @author fskallos
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Welcome, to the World of Tomorrow!");
+	}
+
+}
